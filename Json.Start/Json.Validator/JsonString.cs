5ff60e41136cfed7cbbba072898caacee640168b
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-01-28 13:06:12 +0200

    Uploaded the JSON program

---
+++ Json.Start/Json.Validator/JsonString.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Json
+{
+    public static class JsonString
+    {
+        public static bool IsJsonString(string input)
+        {
+            throw new NotImplementedException();
+    }
+}
