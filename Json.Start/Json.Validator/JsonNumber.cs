27e7d4855062e1674e22b1516c621b336fa81b85
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-01-31 12:55:15 +0200

    Passed the test CanBeZero

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,7 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            throw new NotImplementedException();
+            return int.TryParse(input, out _);
         }
     }
 }
