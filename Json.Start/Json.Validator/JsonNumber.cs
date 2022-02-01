a5466053b2a027c31fe98025a8b684df148c7450
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-02-01 13:35:42 +0200

    Passed test for DoesNotEndWithADot

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -5,8 +5,18 @@ namespace Json
     public static class JsonNumber
     {
         public static bool IsJsonNumber(string input)
-        {
-            return (int.TryParse(input, out _) && input.IndexOf("0") == input.Length - 1 ^ input.IndexOf("0") == -1) || (double.TryParse(input, out _) && input.Contains("."));
+         {
+            if (int.TryParse(input, out _))
+            {
+                return input.IndexOf("0") == input.Length - 1 ^ input.IndexOf("0") == -1;
+            }
+
+            if (!double.TryParse(input, out _))
+            {
+                return false;
+            }
+
+            return input.IndexOf(".") > -1 && input.IndexOf(".") < input.Length - 1;
         }
     }
 }
