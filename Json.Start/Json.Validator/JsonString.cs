0e873000663b784b2d51409bf7e270020065efcf
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-02-11 13:49:56 +0200

    Passed test for AlwaysEndsWithQuotes

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -4,16 +4,39 @@ namespace Json
 {
     public static class JsonString
     {
+        private const int TWO = 2;
+
         public static bool IsJsonString(string input)
         {
-            return IsDoubleQuoted(input);
+            return IsDoubleQuoted(input) && StringIsNotEmptyOrNull(input);
+        }
+
+        static bool StringIsNotEmptyOrNull(string input)
+        {
+            return input != string.Empty || input != null;
         }
 
         static bool IsDoubleQuoted(string input)
         {
             return input.Length > 1
                 && input[0] == '\"'
-                && input[^1] == '\"';
+                && input[^1] == '\"'
+                && CountOccurancesOfCharacter(input, '\"') >= TWO;
+        }
+
+        static int CountOccurancesOfCharacter(string input, char ch)
+        {
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                if (c == ch)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
