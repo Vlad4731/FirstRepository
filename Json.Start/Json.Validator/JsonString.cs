36302273658007c1c66270d4c3d2d1af8181b98f
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-02-11 13:57:15 +0200

    Passed test for IsNotNull

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -8,16 +8,21 @@ namespace Json
 
         public static bool IsJsonString(string input)
         {
-            return IsDoubleQuoted(input) && StringIsNotEmptyOrNull(input);
+            return IsDoubleQuoted(input);
         }
 
-        static bool StringIsNotEmptyOrNull(string input)
+        static bool StringIsEmptyOrNull(string input)
         {
-            return input != string.Empty || input != null;
+            return input == string.Empty || input == null;
         }
 
         static bool IsDoubleQuoted(string input)
         {
+            if (StringIsEmptyOrNull(input))
+            {
+                return false;
+            }
+
             return input.Length > 1
                 && input[0] == '\"'
                 && input[^1] == '\"'
