fbea5c6faf9f7b771f55910a151cc186a7f96ffb
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-11 14:54:56 +0200

    Reverted to empty program

--- FootballRanking/FootballRanking/Ranking.cs
+++ FootballRanking/FootballRanking/Ranking.cs
@@ -1,45 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace FootballRanking
 {
-    class Ranking
+    public class Ranking
     {
-        public struct Team
-        {
-            public string TeamName { get; set; }
-            public string TeamPoints { get; set; }
-        }
-
-        private Team[] footballTeams = new Team[30];
-
-        public void AddTeam(string teamName, int placement)
-        {
-            footballTeams[placement] = new Team { TeamName = teamName };
-        }
-
-        public int GetTeamPlacementByName(string teamName)
-        {
-            for (int i = 0; i < footballTeams.Length; i++)
-            {
-                if (footballTeams[i].TeamName == teamName)
-                {
-                    return i;
-                }
-            }
-
-            return -1;
-        }
-
-        public string GetTeamPlacementByRanking(int inputRanking)
-        {
-            if (!string.IsNullOrEmpty(footballTeams[inputRanking].TeamName))
-            {
-                return footballTeams[inputRanking].TeamName;
-            }
-
-            return null;
-        }
 
     }
 }
--- FootballRanking/FootballRanking/RankingFacts.cs
+++ FootballRanking/FootballRanking/RankingFacts.cs
@@ -5,22 +5,6 @@ namespace FootballRanking
 {
     public class RankingFacts
     {
-        [Fact]
-        public void ReportRankingPlacementOfTeam()
-        {
-            var ranking = new Ranking();
-            ranking.AddTeam("CFR", 1);
 
-            Assert.Equal(1, ranking.GetTeamPlacementByName("CFR"));
-        }
-
-        [Fact]
-        public void ReportTeamFromInputRanking()
-        {
-            var ranking = new Ranking();
-            ranking.AddTeam("CFR", 1);
-
-            Assert.Equal("CFR", ranking.GetTeamPlacementByRanking(1));
-        }
     }
 }
