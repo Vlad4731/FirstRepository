5330554fc2c2708ebcc1efba2cbba531ccc62b66
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-14 13:54:28 +0200

    Added structure to Ranking

--- FootballRanking/FootballRanking/Ranking.cs
+++ FootballRanking/FootballRanking/Ranking.cs
@@ -4,6 +4,34 @@ namespace FootballRanking
 {
     public class Ranking
     {
-        
+        public struct TeamRanking
+        {
+            public readonly Team team;
+            public int teamRanking;
+
+            public TeamRanking(Team inputTeam, int inputRanking)
+            {
+                team = inputTeam;
+                teamRanking = inputRanking;
+            }
+        }
+
+        public TeamRanking[] teamRankings = new TeamRanking[20];
+
+        public Ranking(TeamRanking[] rankings)
+        {
+            rankings = teamRankings;
+        }
+
+        public void AddTeamToRanking(Team team)
+        {
+            teamRankings[0] = new TeamRanking(team, 0);
+        }
+
+        public int ReportTeamPlaceInRanking()
+        {
+            return teamRankings[0].teamRanking;
+        }
+
     }
 }
--- FootballRanking/FootballRanking/RankingFacts.cs
+++ FootballRanking/FootballRanking/RankingFacts.cs
@@ -5,6 +5,13 @@ namespace FootballRanking
 {
     public class RankingFacts
     {
-
+        [Fact]
+        static void TeamCanBeAddedToRanking()
+        {
+            Team echipa = new Team("CFR", 22);
+            Ranking clasament = new Ranking(new Ranking.TeamRanking[1]);
+            clasament.AddTeamToRanking(echipa);
+            Assert.Equal(0, clasament.ReportTeamPlaceInRanking());
+        }
     }
 }
