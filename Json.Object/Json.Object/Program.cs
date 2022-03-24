917e27ba00c0252819a4b91e95714efcaa712693
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-25 00:41:51 +0200

    Removed excess code from Program

--- Json.Object/Json.Object/Program.cs
+++ Json.Object/Json.Object/Program.cs
@@ -7,10 +7,6 @@ namespace Json
         static void Main(string[] args)
         {
             string text = Console.ReadLine();
-            Range interval = new Range('a', 'f');
-            Character x = new Character('x');
-            Console.WriteLine(interval.Match(""));
-            Console.WriteLine(x.Match("xAtraxes"));
 
             var digitPatterns = new IPattern[]
             {
@@ -18,15 +14,10 @@ namespace Json
                 new Range('1', '9')
             };
 
-            var digit = new Choice(
-                new Character('0'),
-                new Range('1', '9')
-            );
-
             foreach (var pattern in digitPatterns)
             {
                 if (pattern.Match(text))
-                    System.Console.WriteLine("Match found");
+                    Console.WriteLine("Match found");
             }
         }
     }
