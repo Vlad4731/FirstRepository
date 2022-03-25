da637cd154cc9cecb25d2eb62d49ba8015088383
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-25 09:57:21 +0200

    Added empty Match class, using IMatch interface

---
+++ Json.Object/Json.Object/Match.cs
@@ -0,0 +1,6 @@
+﻿namespace Json
+{
+    internal class Match : IMatch
+    {
+    }
+}
--- Json.Object/Json.Object/Program.cs
+++ Json.Object/Json.Object/Program.cs
@@ -8,17 +8,6 @@ namespace Json
         {
             string text = Console.ReadLine();
 
-            var digitPatterns = new IPattern[]
-            {
-                new Character('0'),
-                new Range('1', '9')
-            };
-
-            foreach (var pattern in digitPatterns)
-            {
-                if (pattern.Match(text))
-                    Console.WriteLine("Match found");
-            }
         }
     }
 }
