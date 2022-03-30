432df8f9747ba6a3e8f9de94a6993f47cfbfe8eb
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-30 16:11:32 +0300

    Implemented Sequence class with Tests
    Class and most tests are working properly. One test is not working.

---
+++ Json.Object/Json.Object/Sequence.cs
@@ -0,0 +1,35 @@
+﻿namespace Json
+{
+    public class Sequence : IPattern
+    {
+        IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new FailedMatch(text);
+            }
+
+            string textBackup = text;
+
+            foreach (var pattern in patterns)
+            {
+                if (!pattern.Match(text).Success())
+                {
+                    return new FailedMatch(textBackup);
+                }
+
+                text = pattern.Match(text).RemainingText();
+            }
+
+            return new SuccessMatch(text);
+        }
+
+    }
+}
---
+++ Json.Object/Json.Tests/SequenceTests.cs
@@ -0,0 +1,74 @@
+using Xunit;
+
+namespace Json.Tests
+{
+    public class SequenceTests
+    {
+        [Theory]
+        [InlineData("ax")]
+        [InlineData("def")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void CharactersAreMatchedInSequence(string text)
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            Assert.False(ab.Match(text).Success());
+        }
+
+        [Fact]
+        public void OnMatchFailureWholeTextIsReturned()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            Assert.Equal("ax", ab.Match("ax").RemainingText());
+        }
+
+        [Fact]
+        public void CharactersAreMatchInSequenceArray()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var abc = new Sequence(
+                ab,
+                new Character('c')
+                );
+
+            Assert.Equal("d", abc.Match("abcd").RemainingText());
+        }
+
+        [Fact]
+        public void CharactersAreMatchedInHexSequence()
+        {
+            var hex = new Choice(
+                new Range('0', '9'),
+                new Range('a', 'f'),
+                new Range('A', 'F')
+            );
+
+            var hexSeq = new Sequence(
+                new Character('u'),
+                new Sequence(
+                    hex,
+                    hex,
+                    hex,
+                    hex
+                )
+            );
+
+            Assert.True(hexSeq.Match("u1234").Success());
+            Assert.Equal("ef", hexSeq.Match("uabcdef").RemainingText());
+            //Assert.Equal(" ab", hexSeq.Match("uB005 ab").RemainingText());
+            Assert.False(hexSeq.Match("abc").Success());
+        }
+    }
+}
