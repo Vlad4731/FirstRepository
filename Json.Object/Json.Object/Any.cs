3a6d7222965aedcce857b41d66ff882088c10853
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-31 19:36:36 +0300

    Implemented Any class with tests

--- Json.Object/Json.Object/Any.cs
+++ Json.Object/Json.Object/Any.cs
@@ -1,15 +1,30 @@
 ﻿namespace Json
 {
-    public class Any
+    public class Any : IPattern
     {
+        private readonly char[] characters;
+
         public Any(string accepted)
         {
-
+            characters = accepted.ToCharArray();
         }
 
         public IMatch Match(string text)
         {
-            throw new System.NotImplementedException();
+            if(string.IsNullOrEmpty(text))
+            {
+                return new FailedMatch(text);
+            }
+
+            foreach (char c in characters)
+            {
+                if(text[0] == c)
+                {
+                    return new SuccessMatch(text[1..]);
+                }
+            }
+
+            return new FailedMatch(text);
         }
     }
 }
--- Json.Object/Json.Tests/Any.Tests.cs
+++ Json.Object/Json.Tests/Any.Tests.cs
@@ -4,10 +4,31 @@ namespace Json.Tests
 {
     public class AnyTests
     {
-        [Fact]
-        public void Test1()
-        {
 
+        [Fact]
+        public void VoidAndNullAreNotMatched()
+        {
+            var e = new Any("eE");
+            Assert.Equal("", e.Match("").RemainingText());
+            Assert.Null(e.Match(null).RemainingText());
+        }
+
+        [Fact]
+        public void CharactersAreMatchedCorrectly()
+        {
+            var e = new Any("eE");
+            Assert.Equal("a", e.Match("ea").RemainingText());
+            Assert.Equal("a", e.Match("Ea").RemainingText());
+            Assert.Equal("a", e.Match("a").RemainingText());
+        }
+
+        [Fact]
+        public void SymbolsAreMatched()
+        {
+            var sign = new Any("-+");
+            Assert.Equal("3", sign.Match("+3").RemainingText());
+            Assert.Equal("2", sign.Match("-2").RemainingText());
+            Assert.Equal("2", sign.Match("2").RemainingText());
         }
     }
 }
