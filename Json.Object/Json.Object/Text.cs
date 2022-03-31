93ad521ddf2fef908c82a5609788b23725600624
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-03-31 19:40:21 +0300

    Created structure for Text class

---
+++ Json.Object/Json.Object/Text.cs
@@ -0,0 +1,17 @@
+﻿namespace Json
+{
+    public class Text : IPattern
+    {
+        private string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Json.Object/Json.Tests/Text.Tests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace Json.Tests
+{
+    public class TextTests
+    {
+
+        [Fact]
+        public void VoidAndNullAreNotMatched()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
