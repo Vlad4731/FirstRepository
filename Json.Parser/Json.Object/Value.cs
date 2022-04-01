0c24afc51713dd636c6f0fa0f2b9bdc6ae79a250
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-01 22:38:05 +0300

    Fully implemented Value class

---
+++ Json.Parser/Json.Object/Value.cs
@@ -0,0 +1,48 @@
+﻿namespace Json
+{
+    public class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+            var ws = new Any(" \n\t\r");
+
+            var value = new Choice(
+                new String(),
+                new Number(),
+                new Text("true"),
+                new Text("false"),
+                new Text("null"));
+
+            var element = new Sequence(ws, value, ws);
+            var elements = new List(element, new Character(','));
+
+            var array = new Choice(
+                new Sequence(new Character('['), ws, new Character(']')),
+                new Sequence(new Character('['), elements, new Character(']'))
+                );
+
+            var member = new Sequence(ws, new String(), ws, new Character(':'), element);
+            var members = new Choice(
+                member,
+                new List(member, new Character(','))
+                );
+
+            var valueObject = new Choice(
+                new Sequence(new Character('{'), ws, new Character('}')),
+                new Sequence(new Character('{'), ws, members, ws, new Character('}'))
+                );
+
+            value.Add(array);
+            value.Add(valueObject);
+
+            pattern = value;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
