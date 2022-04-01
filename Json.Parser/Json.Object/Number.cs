d529bcdd71265bbfca444054d341ea8e8f3182a7
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-01 16:32:19 +0300

    Fully implemented Number class

---
+++ Json.Parser/Json.Object/Number.cs
@@ -0,0 +1,34 @@
+﻿namespace Json
+{
+    public class Number
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            var digit = new Range('0', '9');
+            var digits = new OneOrMore(digit);
+
+            var signs = new Any("-+");
+            var dot = new Character('.');
+
+            var exponentLetter = new Any("eE");
+
+            var integer = new Choice(
+                new Sequence(new Character('-'), digits),
+                new Sequence(new Character('-'), digit),
+                digit
+            );
+            var exponent = new Optional(new Sequence(exponentLetter, signs, digits));
+            var fraction = new Optional(new Sequence(dot, digits));
+
+            pattern = new Sequence(integer, fraction, exponent);
+
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
