d8954ae309eacc690754798d5e761a7b88446453
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-01 22:59:35 +0300

    Implemented Program, but does not match JSON strings

--- Json.Parser/Json.Object/Program.cs
+++ Json.Parser/Json.Object/Program.cs
@@ -6,7 +6,21 @@ namespace Json
     {
         static void Main(string[] args)
         {
-            string text = Console.ReadLine();
+            string text = System.IO.File.ReadAllText(args[0]);
+
+            var value = new Value();
+
+            if(value.Match(text).Success())
+            {
+                Console.WriteLine("JSON is valid");
+            }
+            else
+            {
+                 Console.WriteLine("JSON is invalid");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
