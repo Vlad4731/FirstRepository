29210c5edfb77e6e4dd20bc4e45433cd0c4f6661
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-05 21:57:15 +0300

    Refactoring Numbers class

--- Json.Parser/Json.Object/Number.cs
+++ Json.Parser/Json.Object/Number.cs
@@ -6,17 +6,13 @@
 
         public Number()
         {
-            var onenine = new Range('1', '9');
-
-            var digit = new Choice(
-                new Character('0'),
-                onenine);
+            var digit = new Range('0', '9');
 
             var digits = new OneOrMore(digit);
 
             var integer = new Choice(
                 new Sequence(new Optional(new Character('-')), digit),
-                new Sequence(new Optional(new Character('-')), onenine, digits)
+                new Sequence(new Optional(new Character('-')), new Character('0'), digits)
             );
 
             var fraction = new Optional(new Sequence(new Character('.'), digits));
