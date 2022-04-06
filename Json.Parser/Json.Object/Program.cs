d096dc2cbe23961111237010462714454e53637a
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-06 14:55:20 +0300

    Added input validation for Main args

--- Json.Parser/Json.Object/Program.cs
+++ Json.Parser/Json.Object/Program.cs
@@ -6,6 +6,12 @@ namespace Json
     {
         static void Main(string[] args)
         {
+
+            if (args == null || args.Length == 0 || string.IsNullOrEmpty(args[0]))
+            {
+                Console.WriteLine("The program must be executed using text file containg a JSON string.");
+            }
+
             string text = System.IO.File.ReadAllText(args[0]);
 
             var value = new Value();
