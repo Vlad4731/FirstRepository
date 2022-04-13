6e117e7dae2f87f0a68447c5e0c7389ea8fd1c59
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-13 15:55:56 +0300

    Added class ObjectArray as object clone of IntArray

--- IntegerArray/IntegerArray/ObjectArray.cs
+++ IntegerArray/IntegerArray/ObjectArray.cs
@@ -2,27 +2,27 @@
 
 namespace IntegerArray
 {
-	public class IntArray
+	public class ObjectArray
 	{
-		private int[] numbers;
+		private object[] objects;
 		public int Count { get; set; } = 0;
 
-		public IntArray()
+		public ObjectArray()
 		{
-			numbers = new int[4];
+			objects = new object[4];
 		}
 
 		public int this[int index]
 		{
-			get => numbers[index];
-			set => numbers[index] = value;
+			get => (int)objects[index];
+			set => objects[index] = value;
 		}
 
 
 		public virtual void Add(int element)
 		{
 			ResizeArray();
-			numbers[Count] = element;
+			objects[Count] = element;
 			Count += 1;
 		}
 
@@ -30,20 +30,20 @@ namespace IntegerArray
         {
 			if ((Count + 1) % 4 == 0)
 			{
-				Array.Resize(ref numbers, numbers.Length + 4);
+				Array.Resize(ref objects, objects.Length + 4);
 			}
 		}
 
-		public bool Contains(int element)
+		public bool Contains(object element)
 		{
 			return IndexOf(element) != -1;
 		}
 
-		public int IndexOf(int element)
+		public int IndexOf(object element)
 		{
 			for (int i = 0; i < Count; i++)
 			{
-				if (numbers[i] == element)
+				if (objects[i].Equals(element))
 				{
 					return i;
 				}
@@ -56,17 +56,18 @@ namespace IntegerArray
 		{
 			if ((Count + 1) % 4 == 0)
 			{
-				Array.Resize(ref numbers, numbers.Length + 4);
+				Array.Resize(ref objects, objects.Length + 4);
 			}
 
-			for (int i = Count - 1; i >= 0; i--)
+			for (int i = objects.Length - 1; i >= 0; i--)
 			{
 				if (i == index)
 				{
-					numbers[i] = element;
+					objects[i] = element;
 					break;
 				}
-				numbers[i] = numbers[i - 1];
+
+				objects[i] = objects[i - 1];
 			}
 		}
 
@@ -75,13 +76,13 @@ namespace IntegerArray
 			Count = 0;
 		}
 
-		public void Remove(int element)
+		public void Remove(object element)
 		{
 			for (int i = 1; i < Count; i++)
 			{
 				bool match = false;
 
-				if (numbers[i] == element)
+				if (objects[i].Equals(element))
 				{
 					match = true;
 					Count -= 1;
@@ -89,19 +90,19 @@ namespace IntegerArray
 
 				if (match == true)
 				{
-					numbers[i] = numbers[i + 1];
+					objects[i] = objects[i + 1];
 				}
 			}
 
-			if (Count <= Count - 1)
+			if (Count <= objects.Length - 1)
 			{
-				numbers[Count + 1] = 0;
+				objects[Count + 1] = 0;
 			}
 		}
 
 		public void RemoveAt(int index)
 		{
-			Remove(numbers[index]);
+			Remove(objects[index]);
 		}
 	}
 }
