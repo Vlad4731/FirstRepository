a64b516be644fbc3562e9299c1c231fd4b97d357
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-04-26 19:24:55 +0300

    Created List class

---
+++ IntegerArray/IntegerArray.Tests/List.Tests.cs
@@ -0,0 +1,106 @@
+using Xunit;
+
+namespace IntegerArray.Tests
+{
+    public class ListTests
+    {
+        List<int> array = new List<int>();
+
+        [Fact]
+        public void ElementsAreAddedToArray()
+        {
+            int[] test = { 1, 2, 3, 4 };
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+            Assert.Equal(array.Count, test.Length);
+        }
+
+        [Fact]
+        public void ArrayLengthIsReturned()
+        {
+            array.Add(1);
+            array.Add(2);
+            Assert.Equal(2, array.Count);
+        }
+
+        [Fact]
+        public void ElementAtIndexIsReturned()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            Assert.Equal(3, array[2]);
+        }
+
+        [Fact]
+        public void ElementIsSet()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array[2] = 5;
+            Assert.Equal(5, array[2]);
+        }
+
+        [Fact]
+        public void ArrayContainsElement()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            Assert.True(array.Contains(3));
+            Assert.False(array.Contains(5));
+        }
+
+        [Fact]
+        public void IndexOfElementIsReturned()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            Assert.Equal(2, array.IndexOf(3));
+        }
+
+        [Fact]
+        public void ElementIsInsertedCorrectly()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Insert(1, 5);
+            Assert.Equal(5, array[1]);
+        }
+
+        [Fact]
+        public void ArrayIsCleared()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Clear();
+            Assert.Equal(0, array.Count);
+        }
+
+        [Fact]
+        public void ElementIsRemoved()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Remove(2);
+            Assert.Equal(2, array.Count);
+        }
+
+        [Fact]
+        public void ElementAtIndexIsRemoved()
+        {
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.RemoveAt(1);
+            Assert.Equal(3, array[1]);
+        }
+    }
+}
---
+++ IntegerArray/IntegerArray/List.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+
+namespace IntegerArray
+{
+	public class List<T> : IEnumerable
+	{
+		private T[] objects;
+		public int Count { get; set; } = 0;
+
+		public List()
+		{
+			objects = new T[4];
+		}
+
+		public object this[int index]
+		{
+			get => objects[index];
+			set => objects[index] = (T)value;
+		}
+
+
+		public virtual void Add(T element)
+		{
+			ResizeArray();
+			objects[Count] = element;
+			Count += 1;
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			for(int i = 0; i < Count; i++)
+            {
+				yield return objects[i];
+            }
+		}
+
+		internal void ResizeArray()
+        {
+			if ((Count + 1) % 4 == 0)
+			{
+				Array.Resize(ref objects, objects.Length + 4);
+			}
+		}
+
+		public bool Contains(T element)
+		{
+			return IndexOf(element) != -1;
+		}
+
+		public int IndexOf(T element)
+		{
+			for (int i = 0; i < Count; i++)
+			{
+				if (objects[i].Equals(element))
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		public void Insert(int index, T element)
+		{
+			if ((Count + 1) % 4 == 0)
+			{
+				Array.Resize(ref objects, objects.Length + 4);
+			}
+
+			for (int i = objects.Length - 1; i >= 0; i--)
+			{
+				if (i == index)
+				{
+					objects[i] = element;
+					break;
+				}
+
+				objects[i] = objects[i - 1];
+			}
+		}
+
+		public void Clear()
+		{
+			Count = 0;
+		}
+
+		public void Remove(T element)
+		{
+			for (int i = 1; i < Count; i++)
+			{
+				bool match = false;
+
+				if (objects[i].Equals(element))
+				{
+					match = true;
+					Count -= 1;
+				}
+
+				if (match == true)
+				{
+					objects[i] = objects[i + 1];
+				}
+			}
+
+			if (Count <= objects.Length - 1)
+			{
+				objects[Count + 1] = default;
+			}
+		}
+
+		public void RemoveAt(int index)
+		{
+			Remove(objects[index]);
+		}
+	}
+}
