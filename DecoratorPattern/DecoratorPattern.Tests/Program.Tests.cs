c5d2d8cb685b47bf350ef7102fded31dd810fef5
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-05-13 00:06:24 +0300

    Program reads text from file

--- DecoratorPattern/DecoratorPattern.Tests/Program.Tests.cs
+++ DecoratorPattern/DecoratorPattern.Tests/Program.Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace DecoratorPattern.Tests
@@ -6,9 +7,10 @@ namespace DecoratorPattern.Tests
     public class ProgramTests
     {
         [Fact]
-        public void Test1()
+        public void TextFile_IsRead_ReturnsText()
         {
-
+            Assert.Equal("Gazpacho",
+                File.ReadAllText(@"C:\Users\Vlad\Documents\GitHub\FirstRepository\DecoratorPattern\DecoratorPattern\bin\Debug\netcoreapp3.1\test.txt"));
         }
     }
 }
--- DecoratorPattern/DecoratorPattern/Program.cs
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.IO;
 
 namespace DecoratorPattern
 {
     internal class Program
     {
+        public static string Adress { get; set; }
+
         static void Main()
         {
+            Adress = Console.ReadLine();
+            Console.WriteLine(File.ReadAllText(Adress));
         }
     }
 }
