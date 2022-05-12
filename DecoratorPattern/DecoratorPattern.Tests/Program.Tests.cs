c424fa45aa9b75e498d4e038bb709147b59f7694
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-05-12 23:43:16 +0300

    Created new solution

---
+++ DecoratorPattern/DecoratorPattern.Tests/Program.Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DecoratorPattern.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DecoratorPattern
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
