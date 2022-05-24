47192776b2bc7195fa84b05f4c2b91bf6ffb8b4c
Vlad4731 <98587390+Vlad4731@users.noreply.github.com>
2022-05-24 17:56:00 +0300

    Added method to choose file manipulation type

--- DecoratorPattern/DecoratorPattern/Program.cs
+++ DecoratorPattern/DecoratorPattern/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 
 namespace DecoratorPattern
 {
@@ -7,12 +6,41 @@ namespace DecoratorPattern
     {
         public static string Adress { get; set; } = @"C:\Users\Vlad\Documents\GitHub\FirstRepository\DecoratorPattern\DecoratorPattern\bin\Debug\netcoreapp3.1\test.txt";
 
+        public static void FileManipulation(ITextFile text, string input)
+        {
+            Console.WriteLine("Type text to be printed to file.");
+
+            switch (input)
+            {
+                case "1":
+                    ITextFile encrypt = new EncryptDecorator(text);
+                    encrypt.WriteToFile();
+                    break;
+
+                case "2":
+                    ITextFile gzip = new GzipDecorator(text);
+                    gzip.WriteToFile();
+                    break;
+
+                case "3":
+                    ITextFile both = new GzipDecorator(text);
+                    both = new EncryptDecorator(both);
+                    both.WriteToFile();
+                    break;
+
+                default:
+                    text.WriteToFile();
+                    break;
+            }
+        }
+
         static void Main()
         {
-            TextFile textFile = new TextFile(Adress);
-            EncryptDecorator encrypt = new EncryptDecorator(textFile);
-            GzipDecorator zippedFile = new GzipDecorator(textFile);
-            zippedFile.WriteToFile();
+            ITextFile textFile = new TextFile(Console.ReadLine());
+
+            Console.WriteLine("Write 0 for plain text output, 1 for encrypted text, 2 for gzipped text and 3 for both encrypted and gzipped");
+
+            FileManipulation(textFile, Console.ReadLine());
         }
     }
 }
